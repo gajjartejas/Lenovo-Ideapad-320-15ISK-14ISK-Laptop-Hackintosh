105fa8f6eb9356734b46c32b12de944c7a2f157c
Gajjar Tejas <gajjartejas26@gmail.com>
2020-11-07 23:12:29 +0530

    extract efi without serial script updated.

---
+++ Tools/README.md
@@ -0,0 +1,31 @@
+
+# Tools
+
+### extract_efi_without_serial.sh
+
+Use script to extract your OpenCore folder without `SystemSerialNumber`, `SystemUUID` and `MLB`.
+
+Uses:
+First get the name of your EFI partition.
+
+```
+diskutil list
+```
+
+Go to `extract_efi_without_serial.sh` directory.
+`First Argument($1)` =  `Device path` = `/dev/disk0s1`
+`Second Argument($2)` = `Device name + OC Folder path` = `/Volumes/ESP/EFI/OC`
+
+```
+./mount_efi.sh /dev/disk0s1  /Volumes/ESP/EFI/OC
+```
+
+This will output the zip of the OpenCore folder.
+```
+OC_OpenCoreVersion_ReleaseDate.zip
+```
+
+Example:
+```
+OC_063_2020_11_02.zip
+```
--- Tools/extract_efi_without_serial.sh
+++ Tools/extract_efi_without_serial.sh
@@ -9,19 +9,16 @@
 EFIOutputFolder=$(dirname $0)
 cd $EFIOutputFolder
 
-sudo diskutil mount /dev/disk0s1
+sudo diskutil mount $1
 cp -R /Volumes/ESP/EFI/OC $EFIOutputFolder
 cd $EFIOutputFolder
 plutil -replace PlatformInfo.DataHub.SystemSerialNumber -string "PLEASE INSERT YOUR OWN" OC/config.plist
 plutil -replace PlatformInfo.DataHub.SystemUUID -string "PLEASE INSERT YOUR OWN" OC/config.plist
-
 plutil -replace PlatformInfo.Generic.SystemSerialNumber -string "PLEASE INSERT YOUR OWN" OC/config.plist
 plutil -replace PlatformInfo.Generic.SystemUUID -string "PLEASE INSERT YOUR OWN" OC/config.plist
 plutil -replace PlatformInfo.Generic.MLB -string "PLEASE INSERT YOUR OWN" OC/config.plist
-
 plutil -replace PlatformInfo.PlatformNVRAM.MLB -string "PLEASE INSERT YOUR OWN" OC/config.plist
-
-Version=$(nvram 4D1FDA02-38C7-4A6A-9CC6-4BCCA8B30102:opencore-version 2>/dev/null | sed -n 's/.*REL-*//p')
-ditto -c -k --sequesterRsrc --keepParent OC $EFIOutputFolder/$Version.zip
+Version=$(nvram 4D1FDA02-38C7-4A6A-9CC6-4BCCA8B30102:opencore-version 2>/dev/null | sed -n 's/.*REL-*//p' | sed  -e 's/-/_/g' )
+ditto -c -k --sequesterRsrc --keepParent OC $EFIOutputFolder/OC_$Version.zip
 rm -rf OC
 exit 0
